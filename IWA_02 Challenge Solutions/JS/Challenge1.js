41c0245fcd21a3d6f4b000b5a3a2a8055df97a91
MinkyVuyi <dialevuyi@gmail.com>
2023-03-29 12:00:11 +0200

    comments

--- IWA_02 Challenge Solutions/JS/Challenge1.js
+++ IWA_02 Challenge Solutions/JS/Challenge1.js
@@ -1,9 +1,9 @@
-/* This is the first name of the user */
+// This is the first name of the user 
 
 const user = 'John'
 
 
-/* This is the last name of the user*/
+//This is the last name of the user
 
 const surname = 'Smith'
 
