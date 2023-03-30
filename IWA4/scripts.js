c050b3a52cbcf89dea580c367e97613df06484e2
MinkyVuyi <dialevuyi@gmail.com>
2023-03-30 14:48:59 +0200

    final changes

--- IWA4/scripts.js
+++ IWA4/scripts.js
@@ -1,23 +1,23 @@
-const year = 2050; //changed date to year = 2050
+const year = 2050; //changed date to year = 2050 and all the other values with date on this file
 let status = 'student';
 let count = 0;
 
-if (year === 2050) { //used === (strict equali )
-  console.log("January", 'New Year’s Day');
+if (year === 2050) { //used (===) strict equality to compare values
+  console.log("January", 'New Year’s Day'); //added a missing closing bracket
   console.log("March", 'Human Rights Day');
-  const month = 'April';
-  console.log(month, 'Family Day');
+  const month = 'April'; // Use const instead of let and declare a variable to store the month
+  console.log(month, 'Family Day'); //changed date to month
   console.log(month, 'Freedom Day');
-  count += 4;
+  count += 4; // Use shorthand operator to increment count 
 
-  if (status === "student") {
+  if (status === "student") { //status is declared deprecated meaning is outdated
     console.log('June', 'Youth Day');
     count += 1;
   }
 
   console.log('August', 'Women’s Day');
   console.log('September', 'Heritage Day');
-  const decMonth = 'December';
+  const decMonth = 'December'; // Declare a variable to store the month
   console.log(decMonth, 'Day of Reconciliation');
   count += 3;
 
@@ -33,3 +33,4 @@ if (year === 2050) { //used === (strict equali )
 console.log('Your status is:', status);
 console.log('The year is:', year);
 console.log('The total holidays is:', count);
+
