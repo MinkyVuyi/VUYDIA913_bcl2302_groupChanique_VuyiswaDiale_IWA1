ee265f875aa9139eee5f1383c49a3980d41505bd
MinkyVuyi <dialevuyi@gmail.com>
2023-03-30 12:23:06 +0200

    done

--- IWA4/scripts.js
+++ IWA4/scripts.js
@@ -2,7 +2,7 @@ const year = 2050; //changed date to year = 2050
 let status = 'student';
 let count = 0;
 
-if (year === 2050) { //used === (strict  )
+if (year === 2050) { //used === (strict equali )
   console.log("January", 'New Year’s Day');
   console.log("March", 'Human Rights Day');
   const month = 'April';
