0b1b83644d0317e7a2bd77462d1d30d934dbfc70
MinkyVuyi <dialevuyi@gmail.com>
2023-03-30 14:56:35 +0200

    changes

--- IWA4/scripts.js
+++ IWA4/scripts.js
@@ -1,36 +1,36 @@
-const year = 2050; //changed date to year = 2050 and all the other values with date on this file
-let status = 'student';
-let count = 0;
 
-if (year === 2050) { //used (===) strict equality to compare values
-  console.log("January", 'New Year’s Day'); //added a missing closing bracket
-  console.log("March", 'Human Rights Day');
-  const month = 'April'; // Use const instead of let and declare a variable to store the month
-  console.log(month, 'Family Day'); //changed date to month
-  console.log(month, 'Freedom Day');
-  count += 4; // Use shorthand operator to increment count 
+const date = 2050 // Use const instead of let and assign a value
+const status = 'student' // Use const instead of let and assign a value
+let count = 0 // Use let to allow changing the value later
 
-  if (status === "student") { //status is declared deprecated meaning is outdated
-    console.log('June', 'Youth Day');
-    count += 1;
+if (date === 2050) { // Use strict equality operator instead of assignment
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	const month = 'April' // Use const instead of let and declare a variable to store the month
+	console.log(month, 'Family Day')
+	console.log(month, 'Freedom Day')
+	count += 4 // Use shorthand operator to increment count
+
+	if (status === "student") { // Use strict equality operator instead of assignment
+	  console.log('June', 'Youth Day')
+		count += 1 // Use shorthand operator to increment count
   }
 
-  console.log('August', 'Women’s Day');
-  console.log('September', 'Heritage Day');
-  const decMonth = 'December'; // Declare a variable to store the month
-  console.log(decMonth, 'Day of Reconciliation');
-  count += 3;
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	const december = 'December' // Declare a variable to store the month
+	console.log(december, 'Day of Reconciliation')
+	count += 3 // Use shorthand operator to increment count
 
-  if (status === "parent") {
-    console.log(decMonth, 'Christmas Day');
-    count += 1;
+	if (status === "parent") { // Use strict equality operator instead of assignment
+	  console.log(december, 'Christmas Day')
+		count += 1 // Use shorthand operator to increment count
   }
 
-  console.log(decMonth, 'Day of Goodwill');
-  count += 1;
+	console.log(december, 'Day of Goodwill')
+	count += 1 // Use shorthand operator to increment count
 }
 
-console.log('Your status is:', status);
-console.log('The year is:', year);
-console.log('The total holidays is:', count);
-
+console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
