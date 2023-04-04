5e8ec9ad74525eb9099caebab4e41c8823e793c5
MinkyVuyi <dialevuyi@gmail.com>
2023-04-04 11:26:25 +0200

    changes

--- IWA6/challenge2/scripts.js
+++ IWA6/challenge2/scripts.js
@@ -8,10 +8,9 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (hourOfDay === 00 && minuteOfDay === 00 && hourOfDay !== undefined && minuteOfDay !== undefined) {
+  const taxAsDecimal = parseInt(tax) / 100;
+  const startingAfterTax = salary * (1 - taxAsDecimal);
+  const balance = startingAfterTax - transport - food - rent;
+  console.log(`R${balance.toFixed(2)}`); //changed toFixed parameter to 2 in the console.log statement
 }
-	
-console.log(balance.toFixed(3))
