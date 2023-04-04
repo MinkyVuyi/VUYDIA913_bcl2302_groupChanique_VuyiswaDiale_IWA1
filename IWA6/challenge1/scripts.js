eed70c37f3b5ae7d9ea18b804efcfcc7de0b1206
MinkyVuyi <dialevuyi@gmail.com>
2023-04-04 14:54:41 +0200

    done

--- IWA6/challenge1/scripts.js
+++ IWA6/challenge1/scripts.js
@@ -3,29 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-/*Used a for loop
-*used let because primaryValid changes
-*initialized let i=0, setting a new variable called i and set it to 0
-*isNaN() is to check if the character is a number
-*primaryPhone[i] selects the current character fo the primaryPhone currently is on 0
-*parseInt(primaryPhone[i]) 
-*/
+const primaryValid = typeof primaryPhone === 'string' && !isNaN(primaryPhone)
+const secondaryValid = typeof secondaryPhone === 'string' && !isNaN(secondaryPhone)
 
-let primaryValid = true
-for (let i = 0; i < primaryPhone.length; i++) {
-  if (isNaN(parseInt(primaryPhone[i]))) { 
-    primaryValid = false // parseInt() to convert the character to a number
-    break
-  }
-}
-
-let secondaryValid = true
-for (let i = 0; i < secondaryPhone.length; i++) {
-  if (isNaN(parseInt(secondaryPhone[i]))) { 
-    secondaryValid = false
-    break
-  }
-}
-
-console.log('Primary phone is a valid numerical string:', primaryValid )
-console.log('Secondary phone is a valid numerical string:', secondaryValid )
+console.log('Primary phone is a valid numerical string:', primaryValid)
+console.log('Secondary phone is a valid numerical string:', secondaryValid)
