599778ef36b79caa4cb2982e9330f8b1faf9f91c
MinkyVuyi <dialevuyi@gmail.com>
2023-04-05 10:20:04 +0200

    commits

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -10,10 +10,11 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
+const owed = parseInt('R' + leoBalance + sarahBalance).toFixed(2)//added .toFixed(2) in order to set decimal numbers to 2
 const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
 const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
 const total = "Total amount owed: "
 const result = leo + sarah + divider + divider + total + owed + divider
 
 console.log(result)
+
