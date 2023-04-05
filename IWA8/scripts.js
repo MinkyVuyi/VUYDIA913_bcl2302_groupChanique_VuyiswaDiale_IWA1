434ffb3210b8d45483cdf2836e4fbb018fc2242c
MinkyVuyi <dialevuyi@gmail.com>
2023-04-05 18:27:22 +0200

    changes

--- IWA8/scripts.js
+++ IWA8/scripts.js
@@ -25,10 +25,10 @@ const leo = {
 	}
 }
 
-const sarah = {
+const sarah = { 
 	name: sarahName + sarahSurname,
 	age: 62,
-	accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
+	accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8', 
 	balance: sarahBalance,
 	address: {
 		number: sarahNumber,
