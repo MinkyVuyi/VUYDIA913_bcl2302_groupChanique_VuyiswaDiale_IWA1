69f368800ff55e023296982c8f1ae14f32183491
MinkyVuyi <dialevuyi@gmail.com>
2023-04-05 16:32:43 +0200

    final changes

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -12,9 +12,9 @@ const divider = '----------------------------------'
 // Only change below this line
 
 const owed = 'R' + (Math.abs(leoBalance) + Math.abs(sarahBalance)).toFixed(2)
-const leo = `${leoName} ${leoSurname} (Owed R${Math.abs(leoBalance).toFixed(2)})`
-const sarah = `${sarahName.trim()} ${sarahSurname} (Owed R${Math.abs(sarahBalance).toFixed(2)})`
-const total = "Total amount owed: "
-const result = `${leo}\n${sarah}\n\n${divider}\n${total}${owed}\n${divider}`
+const leo = `${leoName} ${leoSurname.trim()} (Owed R${Math.abs(leoBalance).toFixed(2)})`
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed R${Math.abs(sarahBalance).toFixed(2)})\n`
+const total = "\n\tTotal amount owed: "
+const result = `${leo}\n${sarah}\n\n${divider}\n${total}${owed}\n\n${divider}`
 
 console.log(result)
