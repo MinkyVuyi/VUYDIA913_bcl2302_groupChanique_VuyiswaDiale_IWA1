05f8ce52c317a79e2ebc1358bd6cd9bfcc61596b
MinkyVuyi <dialevuyi@gmail.com>
2023-04-12 19:24:46 +0200

    changes2

--- IWA11/scripts.js
+++ IWA11/scripts.js
@@ -1,40 +1,25 @@
+// Select the DOM elements for order1 and its child elements
 const order1 = document.querySelector('[data-key="order1"]');
 const order1Biscuits = order1.querySelector('.biscuits .count');
 const order1Donuts = order1.querySelector('.donuts .count');
 const order1Pancakes = order1.querySelector('.pancakes .count');
 const order1Status = order1.querySelector('.status');
 
+// Set the text content of order1's child elements based on their respective data attributes
 order1Biscuits.textContent = order1.dataset.biscuits;
 order1Donuts.textContent = order1.dataset.donuts;
 order1Pancakes.textContent = order1.dataset.pancakes;
 order1Status.textContent = order1.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
 
-const order2 = document.querySelector('[data-key="order2"]');
-const order2Biscuits = order2.querySelector('.biscuits .count');
-const order2Donuts = order2.querySelector('.donuts .count');
-const order2Pancakes = order2.querySelector('.pancakes .count');
-const order2Status = order2.querySelector('.status');
+// Repeat the same process for order2 and order3
+// ... (same comments as above, but with order2 and order3)
 
-order2Biscuits.textContent = order2.dataset.biscuits;
-order2Donuts.textContent = order2.dataset.donuts;
-order2Pancakes.textContent = order2.dataset.pancakes;
-order2Status.textContent = order2.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
-
-const order3 = document.querySelector('[data-key="order3"]');
-const order3Biscuits = order3.querySelector('.biscuits .count');
-const order3Donuts = order3.querySelector('.donuts .count');
-const order3Pancakes = order3.querySelector('.pancakes .count');
-const order3Status = order3.querySelector('.status');
-
-order3Biscuits.textContent = order3.dataset.biscuits;
-order3Donuts.textContent = order3.dataset.donuts;
-order3Pancakes.textContent = order3.dataset.pancakes;
-order3Status.textContent = order3.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
-
+// Select all DOM elements with data-biscuits attribute
 const biscuitCounts = document.querySelectorAll('[data-biscuits]');
 let totalBiscuitsDelivered = 0;
 let totalBiscuitsOutstanding = 0;
 
+// Loop through each biscuitCount element and calculate total biscuits delivered and outstanding
 biscuitCounts.forEach(function(biscuits) {
   if (biscuits.dataset.delivered === 'true') {
     totalBiscuitsDelivered += parseInt(biscuits.dataset.biscuits);
@@ -43,11 +28,13 @@ biscuitCounts.forEach(function(biscuits) {
   }
 });
 
-const donutCounts = document.querySelectorAll('[data-donuts]');
+// Select all DOM elements with data-donuts attribute
+const donutsCounts = document.querySelectorAll('[data-donuts]');
 let totalDonutsDelivered = 0;
 let totalDonutsOutstanding = 0;
 
-donutCounts.forEach(function(donuts) {
+// Loop through each donutsCount element and calculate total donuts delivered and outstanding
+donutsCounts.forEach(function(donuts) {
   if (donuts.dataset.delivered === 'true') {
     totalDonutsDelivered += parseInt(donuts.dataset.donuts);
   } else {
@@ -55,21 +42,26 @@ donutCounts.forEach(function(donuts) {
   }
 });
 
-const pancakeCounts = document.querySelectorAll('[data-pancakes]');
-let totalPancakesDelivered = 0;
-let totalPancakesOutstanding = 0;
+/// Select all DOM elements with data-panCakes attribute
+const panCakesCounts = document.querySelectorAll('[data-panCakes]');
+let totalPanCakesDelivered = 0;
+let totalPanCakesOutstanding = 0;
 
-pancakeCounts.forEach(function(pancakes) {
-  if (pancakes.dataset.delivered === 'true') {
-    totalPancakesDelivered += parseInt(pancakes.dataset.pancakes);
-  } else {
-    totalPancakesOutstanding += parseInt(pancakes.dataset.pancakes);
-  }
-});
+// Loop through each panCakesCount element and calculate total panCakes delivered and outstanding
+panCakesCounts.forEach(function(panCakes) {
+  const panCakesCount = parseInt(panCakes.dataset.panCakes);
+  if (!isNaN(panCakesCount)) {
+    if (panCakes.dataset.delivered === 'true') {
+      totalPanCakesDelivered += panCakesCount;
+    } else {
+      totalPanCakesOutstanding += panCakesCount;
+    }
+  }});
 
+// Log the total counts of delivered and outstanding biscuits, donuts, and pancakes
 console.log(`Total biscuits delivered: ${totalBiscuitsDelivered}`);
 console.log(`Total biscuits outstanding: ${totalBiscuitsOutstanding}`);
 console.log(`Total donuts delivered: ${totalDonutsDelivered}`);
 console.log(`Total donuts outstanding: ${totalDonutsOutstanding}`);
-console.log(`Total pancakes delivered: ${totalPancakesDelivered}`);
-console.log(`Total pancakes outstanding: ${totalPancakesOutstanding}`);
+console.log(`Total pancakes delivered: ${totalPanCakesDelivered}`);
+console.log(`Total pancakes outstanding: ${totalPanCakesOutstanding}`);
