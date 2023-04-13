7a60ec75c4ce768211a276aefbacc560b9d66f30
MinkyVuyi <dialevuyi@gmail.com>
2023-04-13 15:32:38 +0200

    iwa14Challenge2

--- IWA14/Challenge2/scripts.js
+++ IWA14/Challenge2/scripts.js
@@ -1,36 +1,40 @@
-function add = a, b => { a + b }
-
-function multiply = a, b => { a - b }
-
-function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
-}
+function add(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    console.log(added * this.internal.c);
+    return this;
+  }
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
   calculate: internal
 }
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
   calculate: internal
 }
 
-example1.calculate()
-example2.calculate()
+example1.calculate();
+example2.calculate();
