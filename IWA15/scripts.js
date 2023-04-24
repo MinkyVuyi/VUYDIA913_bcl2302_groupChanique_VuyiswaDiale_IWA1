97bc8a5a541525eaafdbb581195c0e110f2e7e56
MinkyVuyi <dialevuyi@gmail.com>
2023-04-24 12:11:52 +0200

    changes IWA15

--- IWA15/scripts.js
+++ IWA15/scripts.js
@@ -7,26 +7,35 @@ const data = {
 }
 
 // Only edit below
+const firstArr = data.lists[0][1];
+const secondArr = data.lists[1][1];
+const thirdArr = data.lists[2][1];
 
-// Accessing the 3 lists of numbers from the data object
-const { first = [1] } = data.lists[0] || {}
-const { second = [1] } = data.lists[1] || {}
-const { third = [1] } = data.lists[2] || {}
+const result = [];
 
-// Defining a function to extract the biggest number from the 3 lists
 const extractBiggest = () => {
-  // Comparing the last element of the first list with the last element of the second list
-  if (first[first.length - 1] > second[second.length - 1]) {
-    return first // If the last element of the 1st list is greater, return the 1st list
-  }
-  // If the last element of the 2nd list is greater, or they are equal
-  // We compare the 3rd list with the second list
-  if (third.length < 1) {
-    return second // If the 3rd list is empty, return the 2nd list
-  }
-
-  return third // Otherwise, return the third list
-}
+	if (firstArr.length === 0) {
+	  return secondArr.length === 0 ? thirdArr.pop() : secondArr.pop();
+	}
+  
+	if (secondArr.length === 0) {
+	  return firstArr.length === 0 ? thirdArr.pop() : firstArr.pop();
+	}
+  
+	if (thirdArr.length === 0) {
+	  return firstArr.length === 0 ? secondArr.pop() : firstArr.pop();
+	}
+  
+	if (firstArr[firstArr.length - 1] >= secondArr[secondArr.length - 1] && firstArr[firstArr.length - 1] >= thirdArr[thirdArr.length - 1]) {
+	  return firstArr.pop();
+	}
+  
+	if (secondArr[secondArr.length - 1] >= thirdArr[thirdArr.length - 1] && secondArr[secondArr.length - 1] >= firstArr[firstArr.length - 1]) {
+	  return secondArr.pop();
+	}
+  
+	return thirdArr.pop();
+  };
 
 // Only edit above
 
--- IWA17/scripts.js
+++ IWA17/scripts.js
@@ -14,89 +14,392 @@ const MONTHS = [
 	'December',
 ];
 
-const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate() // Changed this code to remove the getfullYear
+const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
 
 // Only edit below
 
-//Note, for me to be able to fix this, I had to change most of the code if not all of it.
-const createData = () => { //added const to createData and assigned it to the following variables
-	// Bellow are the variables that were assigned by the createData constant
+const createArray = (length) => {
+	const result = [];
+  
+	for (let i = 0; i < length; i++) {
+	  result.push(null);
+	}
+  
+	return result;
+  };
+  
+  const createData = () => {
 	const current = new Date();
-	const year = current.getFullYear();
-	const month = current.getMonth();
-	const daysInMonth = getDaysInMonth(year, month);
-
-	const weeks = [];
-	let week = [];
-
-	// Find the first day of the month
-	const firstDay = new Date(year, month, 1).getDay();
-
-	// Offset for starting week from Saturday
-	let offset = firstDay < 6 ? firstDay + 1 : 6;
-
-	for (let i = 0; i < offset; i++) {
-		week.push(null); // filled in empty cells for days before the first day of the month
+	current.setDate(1);
+  
+	const startDay = current.getDay();
+	const daysInMonth = getDaysInMonth(current);
+  
+	const weeks = createArray(6);
+	const days = createArray(7);
+	let value = null;
+  
+	for (let weekIndex = 0; weekIndex < weeks.length; weekIndex++) {
+	  value = {
+		week: weekIndex + 1,
+		days: [],
+	  };
+  
+	  for (let dayIndex = 0; dayIndex < days.length; dayIndex++) {
+		const day = dayIndex - startDay + 1 + (weekIndex * 7)
+		const isValid = day > 0 && day <= daysInMonth;
+  
+		value.days.push({
+		  dayOfWeek: dayIndex + 1,
+		  value: isValid ? day : null,
+		});
+	  }
+  
+	  weeks[weekIndex] = value;
 	}
-
-	for (let day = 1; day <= daysInMonth; day++) {
-		week.push(day); // Used a for loop to add each day of the month to the week array
-
-		if (week.length === 7) {
-			// if week array has 7 days, push it to weeks array and start a new week
-			weeks.push(week);
-			week = [];
-		}
-	}
-
-	// Push the last week to weeks array
-	if (week.length > 0) {
-		weeks.push(week);
-	}
-
+  
 	return weeks;
-};
-
-const createCell = (day, isToday) => {
-	const cell = document.createElement('td');
-	cell.classList.add('table__cell');
-
-	if (day !== null) { //This is to check if day is not null
-		cell.innerText = day;
-		if (isToday) {// Check if isToday is true
-			cell.classList.add('table__cell_today');
+  };
+  
+  const addCell = (existing, classString, value) => {
+	return /* html */ `
+	  <td class="${classString}">
+		${value}
+	  </td>
+  
+	  ${existing}
+	`;
+    };
+  
+  const createHtml = (data) => {
+	let result = "";
+  
+	for (let i = 0; i  < data.length; i++) {
+		const week = data[i];
+	  let inner = "";
+	  inner = addCell(inner, "table__cell table__cell_sidebar", `Week ${week.week}`);
+  
+	  for (let j = 0; j < week.days.length; j++){
+		const day = week.day[j];
+		let classString = "table__cell";
+		const currentDate = new Date ();
+		const isToday = currentDate.getDate() === day.value && currentDate.getMonth() === currentDate.getMonth();
+		const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7;
+		const isAlternate = week.week % 2 === 0;
+  
+		if (isToday){
+			classString = `${classString} table__cell_today`;
 		}
-	} 
-	return cell;
-};
-
-const createHtml = () => {
-	const content = document.querySelector('[data-content]');
-	const weeks = createData();
-
-	for (let i = 0; i < weeks.length; i++) {
-		const week = weeks[i];
-		const row = document.createElement('tr');
-
-		// Create and append the week number cell
-		const weekNumber = document.createElement('td');
-		weekNumber.classList.add('table__cell_week');
-		weekNumber.innerText = `Week ${i + 1}`;
-		row.appendChild(weekNumber);
-
-		for (let j = 0; j < week.length; j++) {
-			const day = week[j];
-			const isToday = day === new Date().getDate() && new Date().getMonth() === new Date().getMonth();
-
-			row.appendChild(createCell(day, isToday));
+		if (isWeekend){
+			classString = `${classString} table__cell_weekend`;
 		}
-
-		content.appendChild(row);
+		if (isAlternate){
+			classString = `${classString} table__cell_alternate`;
+		}
+  
+		inner = addCell(inner, classString, day.value || '');
+	  }
+  
+	  result += `<tr>${inner}</tr>`;
 	}
-}
+  
+	return result;
+  };
+ 
 // Only edit above
 
 const current = new Date();
 document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`;
 
-createHtml();
+
+IWA JavaScript Fundamentals (2023)
+IWA_01: Practical Example
+IWA_02: Comments and Logging
+IWA_03: Script Element and Modules
+IWA_04: Variables and Lexical Scope
+IWA_05: Branching Logic
+IWA_06: Type Coercion
+IWA_07: Strings and Logical Operators
+IWA_08: Object Literal Syntax
+IWA_09: Using Objects
+IWA_10: Built-in Objects
+IWA_11: Document Object Model
+IWA_12: Style Object
+IWA_13: Intro to Functions
+IWA_14: Types of Functions
+IWA_15: Arrays and Destructuring
+IWA_16: Using Arrays
+IWA_17: Loops
+IWA_17.1: Lecture
+IWA_17.2: Challenge 1
+IWA_17.3: Project Submission IWA17
+IWA_18: Event Listeners
+IWA_19: Final Capstone Project
+96% complete
+Downloads and Media
+IWA_17.2: Challenge 1
+In the following exercise, you will be required to create a calendar that automatically calculates the current month and day and displays all dates in a grid format.
+
+ 
+
+Note that you will need to organise days in a <html> table by their corresponding day of the week and the week in that month. You are supplied with both an index.html and styles.css file and are not allowed to change these.
+
+ 
+
+You are also supplied with a scripts.js file. Note that you are only allowed to change the code between the two comments. In other words, you will primarily update the following functions:
+
+createData
+createCell
+createHtml
+At the moment the code is broken. However, apply everything you’ve learned thus far about JavaScript to have the table render the current month you are in (dynamically) as in the example below (however note that you are most likely in a different month and not March 2023).
+
+ 
+
+Note that you should also highlight the current day in blue.
+
+ 
+
+ 
+
+Summary:
+
+HTML and CSS are provided. Don’t change these
+Use the provided CSS classes to style the created elements
+The current month and year should be displayed above the calendar
+Each week must be indicated in the left most column (Always starts with Week 1, Week 2 etc.)
+Weeks should alternate between white and grey
+Each date of the month should be displayed under the corresponding weekday
+The current day should be highlighted in blue
+ 
+
+ 
+
+Extra reading material and tools to help you complete this challenge:
+ 
+
+ 
+
+In this challenge you have to create a calendar, now this might sound daunting and a bit scary but believe it or not, you will be able to successfully complete this challenge if you have watched all the lecture videos, worked through the resources given and last but not least here are a few more resources to guide you in the right direction. Use your knowledge of arrays, loops, ternary operators, methods, and objects to complete this challenge.
+
+ 
+
+MDN documentation on topics such as Date, Arrays, and Template literals to understand the correct usage and syntax for the functions and operations used in the code.
+JavaScript Arrays
+Template literals (Template strings) - JavaScript | MDN
+Program Structure :: Eloquent JavaScript
+Learn JavaScript | Codecademy
+ 
+
+ 
+
+
+
+ 
+
+<!-- index.html -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calendar</title>
+        <link rel="stylesheet" href="/styles.css">
+        <script src="/loops.js" defer type="module"></script>
+    </head>
+
+    <body>
+        <h1 data-title>Loading...</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="table__cell table__cell_heading">Week</th>
+                    <th class="table__cell table__cell_heading">Sun</th>
+                    <th class="table__cell table__cell_heading">Mon</th>
+                    <th class="table__cell table__cell_heading">Tue</th>
+                    <th class="table__cell table__cell_heading">Wed</th>
+                    <th class="table__cell table__cell_heading">Thu</th>
+                    <th class="table__cell table__cell_heading">Fri</th>
+                    <th class="table__cell table__cell_heading">Sat</th>
+                </tr>
+            </thead>
+
+            <tbody data-content></tbody>
+        </table>
+    </body>
+</html>
+ 
+
+ 
+
+/* styles.css */
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  flex-direction: column;
+  font-family: sans-serif;
+  background: rgba(0, 0, 0, 0.1);
+  color: rgba(0, 0, 0, 0.9);
+}
+
+.table {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  white-space: nowrap;
+  border-spacing: 0;
+  border-radius: 8px;
+  overflow: hidden;
+  background: rgba(255, 255, 255, 1);
+}
+
+.table__cell {
+  width: 5rem;
+  height: 5rem;
+  align-items: center;
+  justify-content: center;
+  border-collapse: collapse;
+  border-right: 1px solid rgb(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgb(0, 0, 0, 0.1);
+  text-align: center;
+  font-weight: bold;
+}
+
+.table__cell_heading {
+  border-bottom: 1px solid rgb(0, 0, 0, 1);
+  background: rgba(0, 0, 0, 0.9);
+  color: white;
+}
+
+.table__cell_today {
+  color: blue;
+  background: rgba(0, 0, 255, 0.1);
+}
+
+.table__cell_sidebar {
+  color: rgba(0, 0, 0, 0.8);
+  border-right: 1px solid rgb(0, 0, 0, 0.2);
+  font-weight: normal;
+  font-size: 0.8rem;
+}
+
+.table__cell_weekend {
+  font-weight: normal;
+  color: rgba(0, 0, 0, 0.6);
+}
+
+.table__cell_alternate {
+  background: rgba(0, 0, 0, 0.05);
+}
+ 
+
+ 
+
+// scripts.js
+
+const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+
+// Only edit below 
+
+const createArray = (length) => {
+    const result = []
+
+    for (0, i, length) {
+        result
+    }
+}
+
+const createData = () => {
+    const current = new Date
+    current.setDate(1)
+
+    startDay = current.day
+    daysInMonth = getDaysInMonth(current)
+
+    weeks = createArray(5)
+    days = createArray(7)
+    value = null
+
+    for (weekIndex in weeks) {
+        value = [{
+            week: weekIndex + 1,
+            days: []
+        }]
+
+        for (dayIndex in days) {
+            value = dayIndex - startDay
+            isValid = day > 0 && day <= daysInMonth
+
+            result[weekIndex].days = [{
+                dayOfWeek: dayIndex + 1,
+                value: isValid && day,
+            }]
+        }
+    }
+}
+
+const addCell = (existing, classString, value) => {
+    const result = /* html */ `
+        <td ${classString}>
+            ${value}
+        </td>
+
+        ${existing}
+    `
+}
+
+const createHtml = (data) => {
+    let result = ''
+
+    for (week, days in data) {
+        let inner = ""
+        addCell(inner, 'table__cell table__cell_sidebar', 'Week {week}')
+    
+        for (dayOfWeek, value in days) {
+            classString = table__cell
+						isToday = new Date === value
+            isWeekend = dayOfWeek = 1 && dayOfWeek == 7
+            isAlternate = week / 2
+
+            let classString = 'table__cell'
+
+						if (isToday) classString = `${classString} table__cell_today`
+            if (isWeekend) classString === '{classString} table__cell_weekend'
+            if (isAlternate) classString === '{classString} table__cell_alternate'
+            addCell(inner, classString, value)
+        }
+
+        result = `<tr>${inner}</tr>`
+    }
+}
+
+// Only edit above
+
+const current = new Date()
+document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`;
+
+const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data);
